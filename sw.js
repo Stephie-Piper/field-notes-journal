00c3a66b13e097e1be899f98622de52c4dc9cbec
Stephie-Piper <stephie@davepiper.co.uk>
2026-09-10 17:32:00 +0100

    Remove solid interface text selection and force fresh assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='field-notes-v8';
+const CACHE='field-notes-v9';
 const CORE=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./botanicals.svg','./heart-botanical.svg','./botanical-sprig.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));

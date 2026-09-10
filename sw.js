0ea686760372eee7938ef21e10c4ce4680e11762
Stephie-Piper <stephie@davepiper.co.uk>
2026-09-10 16:18:57 +0100

    Add soft paper themes and anatomical illustrations

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='field-notes-v3';
-const CORE=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./botanicals.svg'];
+const CACHE='field-notes-v4';
+const CORE=['./','./index.html','./app.js','./styles.css','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./botanicals.svg','./heart-botanical.svg','./botanical-sprig.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
